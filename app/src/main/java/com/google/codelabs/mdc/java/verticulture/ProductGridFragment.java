1ab5becb956d391802bdcbce59a3da2e04cec5a7
VitusP <vitus.putra@gmail.com>
2018-08-08 12:29:42 -0400

    Add Navigation dropdown

--- app/src/main/java/com/google/codelabs/mdc/java/verticulture/ProductGridFragment.java
+++ app/src/main/java/com/google/codelabs/mdc/java/verticulture/ProductGridFragment.java
@@ -1,5 +1,6 @@
 package com.google.codelabs.mdc.java.verticulture;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -12,12 +13,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.codelabs.mdc.java.verticulture.network.ProductEntry;
 
 import java.util.List;
 
+import io.particle.android.sdk.cloud.ParticleCloudSDK;
 import io.particle.android.sdk.utils.ui.Toaster;
 
 public class ProductGridFragment extends Fragment {
@@ -34,8 +37,9 @@ public class ProductGridFragment extends Fragment {
         // Inflate the layout for this fragment with the ProductGrid theme
         View view = inflater.inflate(R.layout.ver_product_grid_fragment, container, false);
 
-        // Set up the toolbar
+        // Set up the toolbar and listener
         setUpToolbar(view);
+
 
         // Set up the RecyclerView
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
@@ -48,7 +52,6 @@ public class ProductGridFragment extends Fragment {
         recyclerView.addItemDecoration(new ProductGridItemDecoration(largePadding, smallPadding));
 
 
-
         return view;
     }
 
@@ -58,23 +61,16 @@ public class ProductGridFragment extends Fragment {
         if (activity != null) {
             activity.setSupportActionBar(toolbar);
         }
+        //setup navigation listener from NavigationIconClickListener.java
+        toolbar.setNavigationOnClickListener(new NavigationIconClickListener(getContext(), view.findViewById(R.id.product_grid)));
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
         menuInflater.inflate(R.menu.ver_toolbar_menu, menu);
         super.onCreateOptionsMenu(menu, menuInflater);
-    }
 
-    public void UiButtonClicked(int pos){
-        if(pos == 0){
-            System.out.println(pos);
-            ((NavigationHost) getActivity()).navigateTo(new LoginFragment(), false);
-        }else if(pos == 1){
-            System.out.println(pos);
-        }else{
-            System.out.println(pos);
-        }
+
     }
 
 
